2a5a69651c867554889e095eebf090338f7e3c69
Muhammad Arif Sulaksono <marifsulaksono@gmail.com>
2024-10-24 10:18:11 +0700

    implement middleware jwt

--- internal/api/routes/v_1.go
+++ internal/api/routes/v_1.go
@@ -1,11 +1,23 @@
 package routes
 
-import "github.com/marifsulaksono/go-echo-boilerplate/internal/api/controller"
+import (
+	"github.com/marifsulaksono/go-echo-boilerplate/internal/api/controller"
+	"github.com/marifsulaksono/go-echo-boilerplate/internal/api/middleware"
+)
 
-func RouteV1(r *APIVersion) {
-	userController := controller.NewUserController(r.contract.Service.User)
+func RouteV1(av *APIVersion) {
+	userController := controller.NewUserController(av.contract.Service.User)
+	authController := controller.NewAuthController(av.contract.Service.Auth)
 
-	user := r.api.Group("/users")
+	// auth routes
+	auth := av.api.Group("/auth")
+
+	auth.POST("/login", authController.Login)
+
+	// user routes
+	user := av.api.Group("/users")
+	user.Use(middleware.JWTMiddleware()) // use middleware jwt general on user routes
+
 	user.GET("", userController.Get)
 	user.GET("/:id", userController.GetById)
 	user.POST("", userController.Create)
